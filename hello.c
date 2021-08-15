9b7b2d80a427ee2918aca68fcfe3db8a8dd95850
MIRZA OMAR <58821151+omarbaig007212@users.noreply.github.com>
2021-08-15 13:02:14 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,28 +1,20 @@
-%{
-#include<stdio.h>
-int c_count=0;
-%}
-%%
-"/*"[^*/]*"*/" {c_count++;}
-"//".* {c_count++;} 
-%%
-int main( int argc, char **argv)%
+%
 {
 #include <stdio.h>
     int c_count = 0;
     %
 }
 % %
-    "/*"[^* / ] * "*/" { c_count++; }
-"//".* { c_count++; }
+    "/*"[^* / ] * "*/" { c_count++; } /*for single and multiple line comments*/
+"//".* { c_count++; }                 /*for single line comments*/
 % %
     int main(int argc, char **argv)
 {
     FILE *f1, *f2;
-    if (argc > 1)
+    if (argc > 1) /*Pass two filenames for execution*/
     {
-        f1 = fopen(argv[1], "r");
-        if (!f1)
+        f1 = fopen(argv[1], "r"); /*open first file for reading*/
+        if (!f1)                  /*not able to open file*/
         {
             printf("file error \n");
             exit(1);
@@ -39,28 +31,4 @@ int main( int argc, char **argv)%
         printf("Number of Comment Lines: %d\n", c_count);
     }
     return 0;
-}
-
-{
-FILE *f1,*f2;
-if(argc>1)
-{
-f1=fopen(argv[1],"r"); 
-if(!f1) 
-{
-printf("file error \n");
-exit(1);
-}
-yyin=f1;
-f2=fopen(argv[2],"w"); /*open second file for writing*/
-if(!f2) /*not able to open file*/
-{
-printf("Error");
-exit(1);
-}
-yyout=f2;
-yylex();
-printf("Number of Comment Lines: %d\n",c_count);
-}
-return 0;
 }
