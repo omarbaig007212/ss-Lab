674952fde0984c1c009e6db653dc47ee45447f5e
MIRZA OMAR <58821151+omarbaig007212@users.noreply.github.com>
2021-08-15 12:47:48 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,66 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-char op[2], arg1[5], arg2[5], result[5];
-void main()
+%{
+#include<stdio.h>
+int c_count=0;
+%}
+%%
+"/*"[^*/]*"*/" {c_count++;} /*for single and multiple line comments*/
+"//".* {c_count++;} /*for single line comments*/
+%%
+int main( int argc, char **argv)
 {
-    FILE *fp1, *fp2;
-    fp1 = fopen("input.txt", "r");
-    fp2 = fopen("output.txt", "w");
-    while (!feof(fp1))
-    {
-        fscanf(fp1, "%s%s%s%s", result, arg1, op, arg2);
-        if (strcmp(op, "+") == 0)
-        {
-            fprintf(fp2, "\nMOV R0,%s", arg1);
-            fprintf(fp2, "\nADD R0,%s", arg2);
-            fprintf(fp2, "\nMOV %s,R0", result);
-        }
-        if (strcmp(op, "*") == 0)
-        {
-            fprintf(fp2, "\nMOV R0,%s", arg1);
-            fprintf(fp2, "\nMUL R0,%s", arg2);
-            fprintf(fp2, "\nMOV %s,R0", result);
-        }
-        if (strcmp(op, "-") == 0)
-        {
-            fprintf(fp2, "\nMOV R0,%s", arg1);
-            fprintf(fp2, "\nSUB R0,%s", arg2);
-            fprintf(fp2, "\nMOV %s,R0", result);
-        }
-        if (strcmp(op, "/") == 0)
-        {
-            fprintf(fp2, "\nMOV R0,%s", arg1);
-            fprintf(fp2, "\nDIV R0,%s", arg2);
-            fprintf(fp2, "\nMOV %s,R0", result);
-        }
-        if (strcmp(op, "=") == 0)
-        {
-            fprintf(fp2, "\nMOV R0,%s", arg1);
-            fprintf(fp2, "\nMOV %s,R0", result);
-        }
-    }
-    fclose(fp1);
-    fclose(fp2);
-    getch();
+FILE *f1,*f2;
+if(argc>1) /*Pass two filenames for execution*/
+{
+f1=fopen(argv[1],"r"); /*open first file for reading*/
+if(!f1) /*not able to open file*/
+{
+printf("file error \n");
+exit(1);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-T1 -B = ?
-T2 C + D
-T3 T1 * T2
-A T3 = ?
+yyin=f1;
+f2=fopen(argv[2],"w"); /*open second file for writing*/
+if(!f2) /*not able to open file*/
+{
+printf("Error");
+exit(1);
+}
+yyout=f2;
+yylex();
+printf("Number of Comment Lines: %d\n",c_count);
+}
+return 0;
+}
