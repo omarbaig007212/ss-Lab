1bee735c78cc607c9d17be941191fd367c95d79b
MIRZA OMAR <58821151+omarbaig007212@users.noreply.github.com>
2021-08-15 13:01:17 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -6,7 +6,41 @@ int c_count=0;
 "/*"[^*/]*"*/" {c_count++;}
 "//".* {c_count++;} 
 %%
-int main( int argc, char **argv)
+int main( int argc, char **argv)%
+{
+#include <stdio.h>
+    int c_count = 0;
+    %
+}
+% %
+    "/*"[^* / ] * "*/" { c_count++; }
+"//".* { c_count++; }
+% %
+    int main(int argc, char **argv)
+{
+    FILE *f1, *f2;
+    if (argc > 1)
+    {
+        f1 = fopen(argv[1], "r");
+        if (!f1)
+        {
+            printf("file error \n");
+            exit(1);
+        }
+        yyin = f1;
+        f2 = fopen(argv[2], "w"); /*open second file for writing*/
+        if (!f2)                  /*not able to open file*/
+        {
+            printf("Error");
+            exit(1);
+        }
+        yyout = f2;
+        yylex();
+        printf("Number of Comment Lines: %d\n", c_count);
+    }
+    return 0;
+}
+
 {
 FILE *f1,*f2;
 if(argc>1)
